7d142909d34a4f8e7bfb40d997f71dc6525c0725
Katrina Lui <katrinawlui@gmail.com>
2017-09-19 15:47:16 -0700

    Create SessionApiUtil

--- frontend/shuttr.jsx
+++ frontend/shuttr.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { postUser, postSession, deleteSession } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Shuttr</h1>, root);
 });
+
+window.postUser = postUser;
+window.postSession = postSession;
+window.deleteSession = deleteSession;
