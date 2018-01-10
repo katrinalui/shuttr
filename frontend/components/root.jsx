1e3528ad209a2c62b8d4e0a89cccf83b04b7dc83
Katrina Lui <katrinawlui@gmail.com>
2018-01-10 01:24:32 -0800

    Implement Google Analytics

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import GoogleAnalytics from 'react-ga';
 import App from './app';
+
+GoogleAnalytics.initialize('UA-107335093-1');
+
+const history = createBrowserHistory();
+history.listen((location, action) => {
+  GoogleAnalytics.set({ page: location.hash.substr(1) });
+  GoogleAnalytics.pageview(location.hash.substr(1));
+});
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    <HashRouter>
+    <HashRouter history={history}>
       <App />
     </HashRouter>
   </Provider>
