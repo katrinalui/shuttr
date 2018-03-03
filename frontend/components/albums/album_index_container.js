ce4b2f53000d1af37bf0065e8a4368722b35b76d
Katrina Lui <katrinawlui@gmail.com>
2018-03-03 00:44:13 -0800

    Refactor album index

--- frontend/components/albums/album_index_container.js
+++ frontend/components/albums/album_index_container.js
@@ -1,9 +1,70 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import AlbumIndex from './album_index';
+import values from 'lodash/values';
+import { Image, Transformation } from 'cloudinary-react';
+import AlbumIndexItem from './album_index_item';
+import LoadingSpinner from '../loading_spinner';
 import { requestUserAlbums } from '../../actions/album_actions';
 import { requestUser } from '../../actions/user_actions';
 import { selectAllUserAlbums } from '../../reducers/selectors';
-import values from 'lodash/values';
+
+class AlbumIndexContainer extends React.Component {
+  componentWillMount() {
+    const { user, actions, match: { params } } = this.props;
+    actions.requestUserAlbums(params.userId);
+    if (!user) {
+      actions.requestUser(params.userId);
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.userId !== newProps.match.params.userId) {
+      this.props.requestUserAlbums(newProps.match.params.userId);
+      this.props.requestUser(newProps.match.params.userId);
+    }
+  }
+
+  onNewAlbumClick() {
+    this.props.history.push('/albums/new');
+  }
+
+  renderAlbumIndex() {
+    const { albums } = this.props;
+    let albumIndex = <h2>Nothing to see here.</h2>;
+
+    if (albums.length > 0) {
+      const albumItems = albums.map(album =>
+        <AlbumIndexItem key={album.id} album={album}/>
+      );
+      albumIndex = (
+        <div className="albums-index">
+          { albumItems }
+        </div>
+      );
+    }
+
+    return albumIndex;
+  }
+
+  render() {
+    const { albums, loading, user, currentUser } = this.props;
+
+    return (
+      <div className="albums-index-container">
+        { currentUser.id === user.id
+          && <div className="album-button-container">
+              <button className="btn-grey"
+                      onClick={this.onNewAlbumClick.bind(this)}>
+                        New Album</button>
+             </div> }
+        { loading || !user || !albums
+          ? <LoadingSpinner />
+          : this.renderAlbumIndex() }
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => ({
   albums: selectAllUserAlbums(state, ownProps.match.params.userId),
@@ -13,8 +74,7 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestUserAlbums: (userId) => dispatch(requestUserAlbums(userId)),
-  requestUser: (userId) => dispatch(requestUser(userId))
+  actions: bindActionCreators(Object.assign({}, { requestUserAlbums, requestUser }), dispatch)
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(AlbumIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumIndexContainer);
